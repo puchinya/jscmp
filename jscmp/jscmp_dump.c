3ac29736a439bec30e4697b3f8e91cb6c9d387bf
puchinya <xpuchinya@gmail.com>
2015-11-09 21:22:23 +0900

    Add

---
+++ jscmp/jscmp_dump.c
@@ -0,0 +1,67 @@
+
+#include "jscmp_dump.h"
+#include <string.h>
+#include <stdio.h>
+
+static void jscmp_dump_spaces(int cnt)
+{
+	int i;
+	for (i = 0; i < cnt; i++) printf("  ");
+}
+
+static void jscmp_dump_node(jscmp_doc_t *doc, jscmp_node_t node, int first_spaces, int nest)
+{
+	int i;
+	jscmp_node_t cn;
+
+	if (first_spaces) jscmp_dump_spaces(nest);
+
+	switch (jscmp_type(node)) {
+	case JSCMP_TYPE_STR:
+		printf("\"%s\"", jscmp_str_val(doc, node));
+		break;
+	case JSCMP_TYPE_INT:
+		printf("%d", jscmp_int_val(node));
+		break;
+	case JSCMP_TYPE_NULL:
+		printf("null");
+		break;
+	case JSCMP_TYPE_BOOL:
+		printf("%s", jscmp_bool_val(node) ? "true" : "false");
+		break;
+	case JSCMP_TYPE_ARRAY:
+		printf("[\n");
+		for (i = 0, cn = jscmp_array_begin(node); cn != jscmp_array_end(node); cn = jscmp_array_next(cn))
+		{
+			if (i > 0) printf(",\n");
+			jscmp_dump_node(doc, cn, 1, nest + 1);
+			i++;
+		}
+		printf("\n");
+		jscmp_dump_spaces(nest);
+		printf("]");
+		break;
+	case JSCMP_TYPE_OBJECT:
+		printf("{\n");
+		for (i = 0, cn = jscmp_object_begin(node); cn != jscmp_object_end(node); cn = jscmp_object_next(cn))
+		{
+			if (i > 0) printf(",\n");
+
+			jscmp_dump_spaces(nest + 1);
+			printf("\"%s\":", jscmp_str_val(doc, cn));
+
+			jscmp_dump_node(doc, jscmp_object_val(cn), 0, nest + 1);
+			i++;
+		}
+		printf("\n");
+		jscmp_dump_spaces(nest);
+		printf("}");
+		break;
+	}
+}
+
+void jscmp_dump(jscmp_doc_t *doc)
+{
+	jscmp_dump_node(doc, jscmp_root(doc), 1, 0);
+	printf("\n");
+}
---
+++ jscmp/jscmp_dump.h
@@ -0,0 +1,9 @@
+
+#ifndef __JSCMP_DUMP_H
+#define __JSCMP_DUMP_H
+
+#include "jscmp.h"
+
+void jscmp_dump(jscmp_doc_t *doc);
+
+#endif
